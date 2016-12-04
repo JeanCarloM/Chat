56a23e46c3ed82a36f009cbdb0b173098420c7f4
JeanCarloM <toysiito@gmail.com>
2016-12-03 22:16:20 -0600

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -1,8 +1,9 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
-public class ChatServer{
-	
+public class ServidorChat {
+
 	static Socket cliente;
 	static clienteHilo t[] = new clienteHilo[10];
 
@@ -13,11 +14,19 @@ public class ChatServer{
 
 			ServerSocket servidor = new ServerSocket(9000);
 			System.out.println("¡Servidor Activo!!");
+			ArrayList <String> listanick = new ArrayList <String> ();
 
 			while (i < 10){
 
 				cliente = servidor.accept();
-				clienteHilo ch = new clienteHilo(cliente, t);
+				InetAddress localizacion = cliente.getInetAddress();
+				String ip = localizacion.getHostName();
+				System.out.println("Online : " + ip);
+				listanick.add(ip);
+				for (String z:listanick){
+					System.out.println("Array : " + z);
+				}
+				clienteHilo ch =new clienteHilo(cliente, t);
 				t[i] = ch;
 				ch.start();
 				i++;
